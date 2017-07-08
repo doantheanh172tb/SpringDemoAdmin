d5910c6ff13be736bb1dbe470319e29025356599
vTigerCRM_91 <contact.dfr.hta@gmail.com>
2017-07-08 07:05:27 +0700

    init project

---
+++ src/main/java/com/doantheanh/springmycontact/SpringMyContactApplication.java
@@ -0,0 +1,12 @@
+package com.doantheanh.springmycontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyContactApplication.class, args);
+	}
+}
